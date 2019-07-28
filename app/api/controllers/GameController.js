19cadf4244c93ad7ecce844fc60a35663b0eaa55
RomeroCarranzaEmiliano <emilianoromerocarranza@gmail.com>
2019-07-28 11:28:10 -0300

    getWinner utility function

--- app/api/controllers/GameController.js
+++ app/api/controllers/GameController.js
@@ -12,6 +12,7 @@ module.exports = {
 		//variables for comunication with other controllers
 		var boardC = require('./BoardController');
 		var playerC = require('./PlayerController');
+		var u = require('./UtilitiesController');
 		//
 
 		//setup game
@@ -67,71 +68,7 @@ module.exports = {
 					global.games[gameID].time = 5000;
 				}
 
-					//DETECT WINNER
-				//===================================
-				var i;
-				for (i=0; i<game.board.length; i++) {
-					if (game.board[i] == 1) {
-						board1[i] = 1;
-						board2[i] = 0;
-					} else if (game.board[i] == 2) {
-						board2[i] = 1;
-						board1[i] = 0;
-					} else {
-						board1[i] = 0;
-						board2[i] = 0;
-					}
-				}
-
-				sum1[0] = board1[0] + board1[3] + board1[6];
-				sum1[1] = board1[1] + board1[4] + board1[7];
-				sum1[2] = board1[2] + board1[5] + board1[8];
-
-				sum2[0] = board2[0] + board2[3] + board2[6];
-				sum2[1] = board2[1] + board2[4] + board2[7];
-				sum2[2] = board2[2] + board2[5] + board2[8];
-
-
-				if (sum1.includes(3) == true) {
-					winner = 0;
-				}
-				if (sum2.includes(3) == true) {
-					winner = 1;
-				}
-
-					if (winner == -1) {
-					sum1[0] = board1[0] + board1[1] + board1[2];
-					sum1[1] = board1[3] + board1[4] + board1[5];
-					sum1[2] = board1[6] + board1[7] + board1[8];
-
-						sum2[0] = board2[0] + board2[1] + board2[2];
-					sum2[1] = board2[3] + board2[4] + board2[5];
-					sum2[2] = board2[6] + board2[7] + board2[8];
-
-						if (sum1.includes(3) == true) {
-						winner = 0;
-					}
-					if (sum2.includes(3) == true) {
-						winner = 1;
-					}
-				}
-
-					if (winner == -1) {
-					sum1[0] = board1[0] + board1[4] + board1[8];
-					sum1[1] = board1[2] + board1[4] + board1[6];
-
-						sum2[0] = board2[0] + board2[4] + board2[8];
-					sum2[1] = board2[2] + board2[4] + board2[6];
-
-						if (sum1[0] == 3 || sum1[1] == 3) {
-						winner = 0;
-					}
-					if (sum2[0] == 3 || sum2[1] == 3) {
-						winner = 1;
-					}
-				}
-
-					//===================================
+				winner = u.getWinner(gameID);
 
 				if (winner != -1) { //someone won
 					//
--- app/api/controllers/UtilitiesController.js
+++ app/api/controllers/UtilitiesController.js
@@ -8,5 +8,83 @@
 module.exports = {
   
 
+  	getWinner: function(gameID) {
+  		var game = global.games[gameID];
+  		var board1 = [];
+		var board2 = [];
+		var sum1 = [];
+		var sum2 = [];
+		var winner = -1;
+
+  		//DETECT WINNER
+		//===================================
+		var i;
+		for (i=0; i<game.board.length; i++) {
+			if (game.board[i] == 1) {
+				board1[i] = 1;
+				board2[i] = 0;
+			} else if (game.board[i] == 2) {
+				board2[i] = 1;
+				board1[i] = 0;
+			} else {
+				board1[i] = 0;
+				board2[i] = 0;
+			}
+		}
+
+		sum1[0] = board1[0] + board1[3] + board1[6];
+		sum1[1] = board1[1] + board1[4] + board1[7];
+		sum1[2] = board1[2] + board1[5] + board1[8];
+
+		sum2[0] = board2[0] + board2[3] + board2[6];
+		sum2[1] = board2[1] + board2[4] + board2[7];
+		sum2[2] = board2[2] + board2[5] + board2[8];
+
+
+		if (sum1.includes(3) == true) {
+			winner = 0;
+		}
+		if (sum2.includes(3) == true) {
+			winner = 1;
+		}
+
+		if (winner == -1) {
+			sum1[0] = board1[0] + board1[1] + board1[2];
+			sum1[1] = board1[3] + board1[4] + board1[5];
+			sum1[2] = board1[6] + board1[7] + board1[8];
+
+			sum2[0] = board2[0] + board2[1] + board2[2];
+			sum2[1] = board2[3] + board2[4] + board2[5];
+			sum2[2] = board2[6] + board2[7] + board2[8];
+
+			if (sum1.includes(3) == true) {
+				winner = 0;
+			}
+			if (sum2.includes(3) == true) {
+				winner = 1;
+			}
+		}
+
+		if (winner == -1) {
+			sum1[0] = board1[0] + board1[4] + board1[8];
+			sum1[1] = board1[2] + board1[4] + board1[6];
+
+			sum2[0] = board2[0] + board2[4] + board2[8];
+			sum2[1] = board2[2] + board2[4] + board2[6];
+
+			if (sum1[0] == 3 || sum1[1] == 3) {
+				winner = 0;
+			}
+			if (sum2[0] == 3 || sum2[1] == 3) {
+				winner = 1;
+			}
+		}
+
+		//===================================
+
+		return winner;
+
+  	}
+
 };
 
